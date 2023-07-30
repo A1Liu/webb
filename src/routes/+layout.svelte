32fd5513a4f97e853f02ab8e6a65d623a8899604
Albert Liu <albertymliu@gmail.com>
2023-07-29 18:59:04 -0700

    added tauri setup, remove scaffolding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,18 @@
 <script>
-  import Header from "./Header.svelte";
+  import "normalize.css";
   import "./styles.css";
 </script>
 
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
 <div class="app">
-  <Header />
 
   <main>
     <slot />
   </main>
-
-  <footer>
-    <p>
-      visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-    </p>
-  </footer>
 </div>
 
 <style>
@@ -33,23 +31,5 @@
     max-width: 64rem;
     margin: 0 auto;
     box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
   }
 </style>
