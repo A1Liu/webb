136a6538ee4db594b9c4e6ad35b17c385c4060f6
Albert Liu <albertymliu@gmail.com>
2023-10-22 12:27:14 -0700

    meh

--- web/routes/file/+page.svelte
+++ web/routes/file/+page.svelte
@@ -2,28 +2,27 @@
   import { Sheet } from "./cellStore";
   import { page } from "$app/stores";
   import EditCell from "./EditCell.svelte";
-  import Monaco from "$lib/Monaco.svelte";
 
-  const sheet = new Sheet();
-  const layout = sheet.cellLayout;
-  sheet.createCell();
+  let sheet = new Sheet();
+  $: layout = sheet.cellLayout;
+  $: sheet.createCell();
 
   $: path = $page.url.searchParams.get("path");
-  let value = "";
 </script>
 
 <a href="/">go back</a>
+<button on:click={() => (sheet = new Sheet())}>reset</button>
 
 <div>
   PATH = {path}
-
-  <Monaco bind:value />
 </div>
 
 <div class="blarg">
   {#each $layout as cellId}
     <EditCell {sheet} {cellId} />
   {/each}
+
+  <button on:click={() => sheet.createCell()}>+ Add</button>
 </div>
 
 <style>
@@ -31,5 +30,9 @@
     display: flex;
     flex-direction: column;
     gap: 1rem;
+  }
+
+  button {
+    width: fit-content;
   }
 </style>
