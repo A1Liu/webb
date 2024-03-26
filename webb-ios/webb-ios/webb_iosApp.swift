795fe4203cc541f65af1678a3745278e14152a5d
Albert Liu <albertymliu@gmail.com>
2024-03-26 00:20:52 -0700

    fml rip tauri

---
+++ webb-ios/webb-ios/webb_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  webb_iosApp.swift
+//  webb-ios
+//
+//  Created by Albert Liu on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct webb_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
