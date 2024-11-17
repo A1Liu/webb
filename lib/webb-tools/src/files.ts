bb2b5ae88f539623c03e6fe149f095a6bb5d2612
Albert Liu <albertymliu@gmail.com>
2024-11-16 19:55:54 -0800

    Migrate permissions code to webb-tools [1/2] (#44)

---
+++ lib/webb-tools/src/files.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+import { Observable } from "./util";
+import { IndexedDbStore } from "./store";
+
+export enum ResolutionAlgorithm {
+  YataCrdt = "YATA_CRDT",
+  ReadOnly = "READ_ONLY",
+}
+
+export type FileMetadata = z.infer<typeof FileMetadataSchema>;
+export const FileMetadataSchema = z.object({
+  id: z.string(),
+  folder: z.string().array().default([]),
+  lastUpdateDate: z.coerce.date(),
+  expiration: z.coerce.date().nullish(),
+  resolutionAlgorithm: z.nativeEnum(ResolutionAlgorithm),
+});
+
+// Maybe this should still be in zustand?
+// We can make zustand use a better persistence layer maybe
+
+export class FileMetadataStore {
+  private readonly inMemoryCache: Map<string, FileMetadata> = new Map();
+  private readonly persistentStore: IndexedDbStore<FileMetadata>;
+  private readonly cacheHydration: Promise<void>;
+  readonly observable: Observable;
+  private readonly pushUpdate: () => void;
+
+  constructor(
+    dbName: string = "webb-tools",
+    storeName: string = "FileMetadataStore",
+  ) {
+    const persistentStore = new IndexedDbStore<FileMetadata>(dbName, storeName);
+    this.persistentStore = persistentStore;
+    this.cacheHydration = (async () => {
+      for await (const [, file] of persistentStore) {
+        this.inMemoryCache.set(file.id, file);
+      }
+    })();
+
+    [this.pushUpdate, this.observable] = Observable.create();
+  }
+
+  getNote(id: string) {}
+
+  *[Symbol.iterator](): Iterator<FileMetadata> {
+    for (const file of this.inMemoryCache.values()) {
+      yield file;
+    }
+  }
+}
