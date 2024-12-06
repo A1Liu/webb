5c66631c597a16e59c2da87da3d8643ca526cd7e
Albert Liu <albertymliu@gmail.com>
2024-12-06 13:42:48 -0500

    meh

--- web/util/basic.ts
+++
@@ -1,9 +0,0 @@
-// Not sure how I feel about this yet, but the idea is at least interesting.
-// There is some argument to be made that this kind of thing should not be
-// necessary, but at the same time the optional array spread syntax can be
-// quite confusing
-
-export function includeIf<T>(b: boolean, ...t: T[]): T[] {
-  if (!b) return [];
-  return t;
-}
