0dc55e0ca070e01e3bc4d47e366f487ebdef4a41
Albert Liu <albertymliu@gmail.com>
2024-12-07 22:18:37 -0500

    meh

--- native/app/page.tsx
+++ native/app/page.tsx
@@ -1,20 +1,10 @@
 import { TopbarLayout } from "@/components/TopbarLayout";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDeviceProfile } from "../components/state/deviceProfile";
 
 export const dynamic = "force-static";
 
 export default function Home() {
-  const { deviceProfile } = useDeviceProfile();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (deviceProfile) {
-      navigate("/notes");
-    }
-  }, [navigate, deviceProfile]);
-
   return (
     <TopbarLayout
       title={"Home"}
